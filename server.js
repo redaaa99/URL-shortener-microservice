5defdf1f2ba211960844428e69f55aa0e0b20ded
System <none>
2017-08-12 21:55:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var mongodb = require('mongodb');
 var databaseUrl = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
 var options = { keepAlive: 300000, connectTimeoutMS: 30000 }
 
-function findThisUrl(url,db,response)
+function findThisUrl(url,db,response,callback)
 {
-  db.collection('urls').findOne({"redirectTo": url},function(result){
+  db.collection('urls').findOne({"redirectTo": url},function(err,result){
+    if(err){throw err;}
     console.log(result);
     if(result)
       {
@@ -42,8 +43,10 @@ function findThisUrl(url,db,response)
              
         });*/
       }
-    
+    callback(result);
   });
+
+  
 }
 
 app.use(express.static('public'));
@@ -53,7 +56,7 @@ mongodb.MongoClient.connect(databaseUrl,options,function (err, db){
   app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.get("/:id(\\d+)", function (request, response) {
+  app.get("/:id(\\d+)", function (request, response) {
 var id =request.params.id;
 /*
 if id exists in database redirect to website;
@@ -61,21 +64,23 @@ else response.json({error : "Cannot find this URL in database"});
 */
 response.send(id);
 });    
-app.get(/^\/(.+)/, function (request, response) {
+  app.get(/^\/(.+)/, function (request, response) {
 var url = request.params["0"].toString();
 var reg =/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
 if(reg.test(url))
 {
 
-  findThisUrl(url,db,response);
+  findThisUrl(url,db,response,function() {
+    db.close();
+  });
 }
 else
 {
   response.json({error : "Wrong url format, make sure you have a valid protocol and real site."});
 }
 }); 
-app.listen(process.env.PORT);
-db.close();
+  app.listen(process.env.PORT);
+
     
 });
                     
